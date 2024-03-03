a4f810af3aa22a06db358e6343cd567574556a02
Niklas Haug <niklas.haug@hs-augsburg.de>
2024-03-03 18:16:32 +0100

    got rid of generated texts & styles

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import CarbonBadge from './components/CarbonBadge.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import CarbonBadge from "./components/CarbonBadge.vue";
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="vue-carbonbadge" />
   <CarbonBadge />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
