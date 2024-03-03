7219fa76993c5175d70025c902a3c319787da80d
Niklas Haug <niklas.haug@hs-augsburg.de>
2024-03-03 18:11:38 +0100

    linted CarbonBadge component file

--- src/components/CarbonBadge.vue
+++ src/components/CarbonBadge.vue
@@ -1,31 +1,32 @@
 <script setup lang="ts">
-  const props = defineProps<{
-    dark?: boolean
-  }>();
+const props = defineProps<{
+  dark?: boolean;
+}>();
 
-  import {onMounted, ref} from "vue";
+import { onMounted, ref } from "vue";
 
-  const darkColorSchemaMediaQuery = window?.matchMedia('(prefers-color-scheme: dark)')
-  const userDefinedDarkProperty = props.dark !== undefined;
-  const darkMode = ref(userDefinedDarkProperty ? props.dark : darkColorSchemaMediaQuery?.matches)
+const darkColorSchemaMediaQuery = window?.matchMedia(
+  "(prefers-color-scheme: dark)",
+);
+const userDefinedDarkProperty = props.dark !== undefined;
+const darkMode = ref(
+  userDefinedDarkProperty ? props.dark : darkColorSchemaMediaQuery?.matches,
+);
 
-  // launch listener
-  if (darkColorSchemaMediaQuery) {
-    darkColorSchemaMediaQuery?.addEventListener('change', e =>
-        darkMode.value = e.matches
-    )
-  }
+// launch listener
+if (darkColorSchemaMediaQuery) {
+  darkColorSchemaMediaQuery?.addEventListener(
+    "change",
+    (e) => (darkMode.value = e.matches),
+  );
+}
 
- onMounted(() => {
-   /*@ts-ignore*/
-   import('website-carbon-badges/b.min')
- })
+onMounted(() => {
+  /*@ts-ignore*/
+  import("website-carbon-badges/b.min");
+});
 </script>
 
 <template>
   <div id="wcb" :class="[darkMode ? 'wcb-d' : '', 'carbonbadge']"></div>
 </template>
-
-<style scoped>
-
-</style>
